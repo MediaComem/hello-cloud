c5fffa8b891b480be0f03e8985f0cfd3cf2dfc73
Simon Oulevay <simon.oulevay@gmail.com>
2021-06-14 14:44:17 +0200

    Add GET /config route

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 
+const { maxGreetings } = require('../config');
 const { version } = require('../package.json');
 
 const router = express.Router();
 
 router.get('/', function(req, res) {
-  res.json({
+  res.send({
+    version
+  });
+});
+
+router.get('/config', function(req, res) {
+  res.send({
+    maxGreetings,
     version
   });
 });
